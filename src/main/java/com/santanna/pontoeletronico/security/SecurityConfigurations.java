3a3f1811d8404c58daa1faf0307ccd34c01718ce
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-06-02 16:24:06 -0300

    remove imports

--- src/main/java/com/santanna/pontoeletronico/security/SecurityConfigurations.java
+++ src/main/java/com/santanna/pontoeletronico/security/SecurityConfigurations.java
@@ -1,5 +1,5 @@
 package com.santanna.pontoeletronico.security;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
